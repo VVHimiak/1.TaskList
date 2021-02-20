6ff4f15cc52c23a781fd32f0b22c145e73881df6
Volodymyr Himiak <vv.himiak@gmail.com>
2021-02-20 14:01:13 +0200

    Delete & Filter Tasks

--- index.js
+++ index.js
@@ -3,16 +3,24 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('.filter');
+const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 //Load all event Listeners
 loadEventListeners();
 
 function loadEventListeners(){
+  //Add task event
   form.addEventListener('submit', addTask);
+  //Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
+//addTask function
 function addTask(e){
 //   let val = taskInput.value;
 //   let storage = JSON.parse(localStorage.getItem('tasks'));
@@ -44,4 +52,44 @@ taskList.appendChild(li);
 //Clear input
 taskInput.value = '';
   e.preventDefault();
+}
+
+//removeTask function
+function removeTask(e){
+
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure')){
+    e.target.parentElement.parentElement.remove();
+    }
+  }
+  console.log(e.target);
+  e.preventDefault();
+}
+
+//clearTask function
+function clearTasks(e){
+  //taskList.innerHTML = '';
+
+  //Faster
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//filterTasks function
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+
+      task.style.display = 'block';
+      console.log(task)
+    } else {
+      task.style.display = 'none';
+
+    }
+
+  })
+  
 }
